7d260a5c8395c6c1298a7475b825f0a7cccc823a
Aayan Shaikh <shaikhaayan690@gmail.com>
2024-05-04 12:34:32 -0400

    abc

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,12 @@ file_path = (".env")
 with open(file_path, 'r') as file:
     # Perform operations on the file
     for line in file:
-        # key = (line.strip()) 
+         key = (line.strip()) 
 
 
 client = OpenAI(api_key=key)
 completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
+  #model="gpt-3.5-turbo",
   messages=[
     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
     {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
